6ce38a519e7149728cf79dab9b2a497b2cf386e9
Rafael Corsi <corsiferrao@gmail.com>
2016-05-18 18:28:08 -0300

    atividade 14

---
+++ 14-ADC/ILI93XX_EXAMPLE1/src/ili93xx_example.c
@@ -0,0 +1,334 @@
+#include "asf.h"
+#include "stdio_serial.h"
+#include "conf_board.h"
+#include "conf_clock.h"
+#include "smc.h"
+#include <math.h>
+
+/************************************************************************/
+/* ADC                                                                     */
+/************************************************************************/
+
+/** Size of the receive buffer and transmit buffer. */
+#define BUFFER_SIZE     (100)
+/** Reference voltage for ADC,in mv. */
+#define VOLT_REF        (3300)
+/* Tracking Time*/
+#define TRACKING_TIME    1
+/* Transfer Period */
+#define TRANSFER_PERIOD  1
+/* Startup Time*/
+#define STARTUP_TIME ADC_STARTUP_TIME_4
+
+/** The maximal digital value */
+#define MAX_DIGITAL     (4095)
+
+/* Redefinir isso */
+#define ADC_POT_CHANNEL 1
+
+/************************************************************************/
+/* LCD                                                                  */
+/************************************************************************/
+/** Chip select number to be set */
+#define ILI93XX_LCD_CS      1
+
+struct ili93xx_opt_t g_ili93xx_display_opt;
+
+/************************************************************************/
+/* prototype                                                            */
+/************************************************************************/
+
+void configure_LCD();
+void configure_ADC();
+
+/************************************************************************/
+/* Configs                                                              */
+/************************************************************************/
+void configure_LCD(){
+	/** Enable peripheral clock */
+	pmc_enable_periph_clk(ID_SMC);
+
+	/** Configure SMC interface for Lcd */
+	smc_set_setup_timing(SMC, ILI93XX_LCD_CS, SMC_SETUP_NWE_SETUP(2)
+	| SMC_SETUP_NCS_WR_SETUP(2)
+	| SMC_SETUP_NRD_SETUP(2)
+	| SMC_SETUP_NCS_RD_SETUP(2));
+	smc_set_pulse_timing(SMC, ILI93XX_LCD_CS, SMC_PULSE_NWE_PULSE(4)
+	| SMC_PULSE_NCS_WR_PULSE(4)
+	| SMC_PULSE_NRD_PULSE(10)
+	| SMC_PULSE_NCS_RD_PULSE(10));
+	smc_set_cycle_timing(SMC, ILI93XX_LCD_CS, SMC_CYCLE_NWE_CYCLE(10)
+	| SMC_CYCLE_NRD_CYCLE(22));
+	#if ((!defined(SAM4S)) && (!defined(SAM4E)))
+	smc_set_mode(SMC, ILI93XX_LCD_CS, SMC_MODE_READ_MODE
+	| SMC_MODE_WRITE_MODE
+	| SMC_MODE_DBW_8_BIT);
+	#else
+	smc_set_mode(SMC, ILI93XX_LCD_CS, SMC_MODE_READ_MODE
+	| SMC_MODE_WRITE_MODE);
+	#endif
+	/** Initialize display parameter */
+	g_ili93xx_display_opt.ul_width = ILI93XX_LCD_WIDTH;
+	g_ili93xx_display_opt.ul_height = ILI93XX_LCD_HEIGHT;
+	g_ili93xx_display_opt.foreground_color = COLOR_BLACK;
+	g_ili93xx_display_opt.background_color = COLOR_WHITE;
+
+	/** Switch off backlight */
+	aat31xx_disable_backlight();
+
+	/** Initialize LCD */
+	ili93xx_init(&g_ili93xx_display_opt);
+
+	/** Set backlight level */
+	aat31xx_set_backlight(AAT31XX_AVG_BACKLIGHT_LEVEL);
+
+	ili93xx_set_foreground_color(COLOR_WHITE);
+	ili93xx_draw_filled_rectangle(0, 0, ILI93XX_LCD_WIDTH,
+	ILI93XX_LCD_HEIGHT);
+	/** Turn on LCD */
+	ili93xx_display_on();
+	ili93xx_set_cursor_position(0, 0);
+}
+
+void configure_ADC(void){
+	
+	/* Enable peripheral clock. */
+	pmc_enable_periph_clk(ID_ADC);
+	
+	/* Initialize ADC. */
+	/*
+	 * Formula: ADCClock = MCK / ( (PRESCAL+1) * 2 )
+	 * For example, MCK = 64MHZ, PRESCAL = 4, then:
+	 * ADCClock = 64 / ((4+1) * 2) = 6.4MHz;
+	 */
+	/* Formula:
+	 *     Startup  Time = startup value / ADCClock
+	 *     Startup time = 64 / 6.4MHz = 10 us
+	 */
+	adc_init(ADC, sysclk_get_cpu_hz(), 6400000, STARTUP_TIME);
+	
+	/* Formula:
+	 *     Transfer Time = (TRANSFER * 2 + 3) / ADCClock
+	 *     Tracking Time = (TRACKTIM + 1) / ADCClock
+	 *     Settling Time = settling value / ADCClock
+	 *
+	 *     Transfer Time = (1 * 2 + 3) / 6.4MHz = 781 ns
+	 *     Tracking Time = (1 + 1) / 6.4MHz = 312 ns
+	 *     Settling Time = 3 / 6.4MHz = 469 ns
+	 */
+	adc_configure_timing(ADC, TRACKING_TIME	, ADC_SETTLING_TIME_3, TRANSFER_PERIOD);
+
+	/*
+	* Configura trigger por software
+	*/ 
+	adc_configure_trigger(ADC, ADC_TRIG_SW, 0);
+
+	/*
+	* Checa se configuração 
+	*/
+	// adc_check(ADC, sysclk_get_cpu_hz());
+
+	/* Enable channel for potentiometer. */
+	adc_enable_channel(ADC, ADC_POT_CHANNEL);
+
+	/* Enable ADC interrupt. */
+	NVIC_EnableIRQ(ADC_IRQn);
+
+	/* Start conversion. */
+	adc_start(ADC);
+
+	/* Enable PDC channel interrupt. */
+	adc_enable_interrupt(ADC, ADC_ISR_RXBUFF);
+}
+
+
+
+
+/**
+ *  Configure Timer Counter 0 to generate an interrupt every 250ms.
+ */
+// [main_tc_configure]
+static void configure_tc(uint32_t freq)
+{
+	/*
+	* Aqui atualizamos o clock da cpu que foi configurado em sysclk init
+	*
+	* O valor atual est'a em : 120_000_000 Hz (120Mhz)
+	*/
+	uint32_t ul_sysclk = sysclk_get_cpu_hz();
+	
+	/*
+	*	Ativa o clock do periférico TC 0
+	* 
+	*/
+	pmc_enable_periph_clk(ID_TC0);
+
+	/*
+	* Configura TC para operar no modo de comparação e trigger RC
+	* devemos nos preocupar com o clock em que o TC irá operar !
+	*
+	* Cada TC possui 3 canais, escolher um para utilizar.
+	*
+	* Configurações de modo de operação :
+	*	#define TC_CMR_ABETRG (0x1u << 10) : TIOA or TIOB External Trigger Selection 
+	*	#define TC_CMR_CPCTRG (0x1u << 14) : RC Compare Trigger Enable 
+	*	#define TC_CMR_WAVE   (0x1u << 15) : Waveform Mode 
+	*
+	* Configurações de clock :
+	*   #define  TC_CMR_TCCLKS_TIMER_CLOCK1 : Clock selected: internal MCK/2 clock signal 
+	*	#define  TC_CMR_TCCLKS_TIMER_CLOCK2 : Clock selected: internal MCK/8 clock signal 
+	*	#define  TC_CMR_TCCLKS_TIMER_CLOCK3 : Clock selected: internal MCK/32 clock signal 
+	*	#define  TC_CMR_TCCLKS_TIMER_CLOCK4 : Clock selected: internal MCK/128 clock signal
+	*	#define  TC_CMR_TCCLKS_TIMER_CLOCK5 : Clock selected: internal SLCK clock signal 
+	*
+	*	MCK		= 120_000_000
+	*	SLCK	= 32_768		(rtc)
+	*
+	* Uma opção para achar o valor do divisor é utilizar a funcao
+	* tc_find_mck_divisor()
+	*/
+	tc_init(TC0,0,TC_CMR_CPCTRG | TC_CMR_TCCLKS_TIMER_CLOCK5);
+	
+	/*
+	* Aqui devemos configurar o valor do RC que vai trigar o reinicio da contagem
+	* devemos levar em conta a frequência que queremos que o TC gere as interrupções
+	* e tambem a frequencia com que o TC está operando.
+	*
+	* Devemos configurar o RC para o mesmo canal escolhido anteriormente.
+	*	
+	*   ^ 
+	*	|	Contador (incrementado na frequencia escolhida do clock)
+	*   |
+	*	|	 	Interrupcao	
+	*	|------#----------- RC
+	*	|	  /
+	*	|   /
+	*	| /
+	*	|-----------------> t
+	*
+	*
+	*/
+	tc_write_rc(TC0,0,TC_CMR_TCCLKS_TIMER_CLOCK5/freq);
+	
+	/*
+	* Devemos configurar o NVIC para receber interrupções do TC 
+	*/
+	NVIC_EnableIRQ((IRQn_Type) ID_TC0);
+	
+	/*
+	* Opções possíveis geradoras de interrupção :
+	* 
+	* Essas configurações estão definidas no head : tc.h 
+	*
+	*	#define TC_IER_COVFS (0x1u << 0)	Counter Overflow 
+	*	#define TC_IER_LOVRS (0x1u << 1)	Load Overrun 
+	*	#define TC_IER_CPAS  (0x1u << 2)	RA Compare 
+	*	#define TC_IER_CPBS  (0x1u << 3)	RB Compare 
+	*	#define TC_IER_CPCS  (0x1u << 4)	RC Compare 
+	*	#define TC_IER_LDRAS (0x1u << 5)	RA Loading 
+	*	#define TC_IER_LDRBS (0x1u << 6)	RB Loading 
+	*	#define TC_IER_ETRGS (0x1u << 7)	External Trigger 
+	*/
+	tc_enable_interrupt(TC0,0,TC_IER_CPCS);
+	
+	tc_start(TC0,0);
+ }
+
+
+
+/************************************************************************/
+/* Interruptions                                                        */
+/************************************************************************/
+
+/**
+ * \brief Timmer handler (100ms) starts a new conversion.
+ */
+void TC0_Handler(void)
+{
+	volatile uint32_t ul_dummy;
+
+	/* Clear status bit to acknowledge interrupt */
+	ul_dummy = tc_get_status(TC0,0);
+
+	/* Avoid compiler warning */
+	UNUSED(ul_dummy);
+	
+	if (adc_get_status(ADC) & (1 << ADC_POT_CHANNEL)) {
+		adc_start(ADC);
+	}
+}
+
+/**
+ * \brief ADC interrupt handler.
+ * Entramos aqui quando a conversao for concluida.
+ */
+void ADC_Handler(void)
+{
+	uint32_t resistencia ;
+	uint32_t resistencia2 ;
+
+	if ((adc_get_status(ADC) & ADC_ISR_RXBUFF) == ADC_ISR_RXBUFF) {
+	// cada bit =0,00081 V
+		resistencia = adc_get_channel_value(ADC, 0);
+		resistencia = resistencia*0.00081;
+	}
+}
+
+void UpdateResIndicator(uint32_t res){
+	int max = 10;
+	int min = 0;
+	int x;
+	int y;
+	int angle;
+	int radius = 40;
+	//Clean screen
+	ili93xx_set_foreground_color(COLOR_WHITE);
+	ili93xx_draw_filled_circle(ILI93XX_LCD_WIDTH/2,ILI93XX_LCD_HEIGHT*0.65,95);
+	//Update indecator
+	ili93xx_set_foreground_color(COLOR_BLACK);
+	if(res<min || res>max){
+		ili93xx_draw_string(10, ILI93XX_LCD_HEIGHT*0.6, (uint8_t *)"Resistência inválida");
+	}
+	else{
+		
+		angle=res/(max-min)*3.141592653;
+		
+		x = cos(3.141592653-angle)*radius;
+		y = sin(angle)*radius;
+		ili93xx_draw_line(ILI93XX_LCD_WIDTH/2, ILI93XX_LCD_HEIGHT*0.65, x, y);
+	}
+}
+/**
+ * \brief Application entry point for smc_lcd example.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+	sysclk_init();
+	board_init();
+
+	configure_LCD();
+	//configure_ADC();
+	
+	configure_tc(1);
+	ili93xx_set_foreground_color(COLOR_BLACK);
+	ili93xx_draw_string(10, 20, (uint8_t *)"Rodrigo 13.04537-7");
+	
+	ili93xx_draw_string(10, 40, (uint8_t *)"Eric    13.01703-9");
+	
+	ili93xx_draw_string(10, 60, (uint8_t *)"Thiago  12.03287-5");
+
+	ili93xx_draw_line(0, 90, 400, 90);
+
+	ili93xx_draw_line(0, 100, 400, 100);
+
+	ili93xx_draw_circle(ILI93XX_LCD_WIDTH/2,ILI93XX_LCD_HEIGHT*0.65,100);
+	int res;
+	ili93xx_draw_circle(ILI93XX_LCD_WIDTH/2,ILI93XX_LCD_HEIGHT*0.65,100);
+	UpdateResIndicator(4);
+	while (1) {
+		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+	}
+}
+
